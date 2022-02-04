af1545b1c1366f34e70c2dec19c2cc1eebda822f
HeroGamers <hero@herogamers.dev>
2022-02-04 10:12:40 +0100

    Initial commit

---
+++ machineprog/binary.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def binary_to_decimal(binary: str):
+    """Returns the decimal value of the binary given."""
+    return int(binary)
+
+
+def decimal_to_binary(decimal: int):
+    """Returns the binary string of the decimal value given."""
+    return bin(decimal)
+
+
+def adu(binary_1: str, binary_2: str):
+    """Adds two binary strings together."""
+    return
+
+
+ADU = adu
