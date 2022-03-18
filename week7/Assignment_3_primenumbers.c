9b1a0672d5799c8e9332d9f77ca8fce53638a6f6
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2022-03-18 08:51:22 +0100

    some c shit

---
+++ week7/Assignment_3_primenumbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < 0; i++){
+        
+        for (int j = 3; j < sqrt(i); j += 2){
+
+        }
+
+    }
+}
