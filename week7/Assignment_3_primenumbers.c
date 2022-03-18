98ba6f3db5b64d5ef91386ed180d4e0ce4ee95ae
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2022-03-18 09:43:32 +0100

    assignment 3 works

--- week7/Assignment_3_primenumbers.c
+++ week7/Assignment_3_primenumbers.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-#include <math.h>
 
 void main(){
-    int n;
-    scanf("%d", &n);
-    for (int i = 0; i < 0; i++){
-        
-        for (int j = 3; j < sqrt(i); j += 2){
-
+    int min, max, isPrime;
+    printf("input a minimum and maximum value\n");
+    scanf("%d %d", &min, &max);
+    printf("input was min = %d max = %d \n", min, max);
+    for (int i = min+1; i < max; i++){
+        isPrime = 1;
+        for (int j = 2; j*j <= i; ++j ){
+            if (i%j==0){
+                isPrime = 0;
+                break;
+            }
         }
-
+        if (isPrime == 1){
+            printf("%d \n", i);
+        }
     }
 }
