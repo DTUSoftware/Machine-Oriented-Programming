03eff4d28f852c4ab251a6a1bdae46ac79d2358e
HeroGamers <hero@herogamers.dev>
2022-04-01 10:25:18 +0200

    Comments for what we need to improve

---
+++ projects/project_2/commands.h
@@ -0,0 +1,10 @@
+#ifndef MACHINE_ORIENTED_PROGRAMMING_PROJECT_2_COMMANDS_H
+#define MACHINE_ORIENTED_PROGRAMMING_PROJECT_2_COMMANDS_H
+
+// Command enum
+
+// function to print/draw the terminal window / GUI/CLI [SW]
+
+// function to handle commands
+
+#endif //MACHINE_ORIENTED_PROGRAMMING_PROJECT_2_COMMANDS_H
--- projects/project_2/yukon.c
+++ projects/project_2/yukon.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include "commands.h"  // Import the commands
+
+// LinkedList and Node struct
+
+// Card struct - bool value for face up or down
+
+// Game phase enum (startup, etc.)
+
+// function to check if card from one column can move onto other column
+// - get other column, get tail/head of linked list, check requirements in rules
+
 
 int main() {
+    // the moves/executed commands can be stored in a linked list
+    // in order to be able to undo commands
+
+    // the commands that have been undone can be stored in a linked list
+    // in order to be able to redo commands
+
+    // the 7 columns can be represented as an array of linked lists
+
+    // the four foundations can be represented as an array of linked lists
     return 0;
 }
