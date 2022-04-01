f435ced9dbf7f4103fd1f7837a50a2503abf18da
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2022-04-01 11:52:00 +0200

    some shit was made

--- week9/1637.c
+++ week9/1637.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#define MAX_SIZE 10
 
 int main() {
+    int index;
+    int array[MAX_SIZE];
+    int limit;
 
-    return 0;
+    printf("Enter a limit (integer):");
+    scanf("%d", &limit);
+    if (limit >= MAX_SIZE){
+        printf("limit too big try something smalle than %d", MAX_SIZE);
+        scanf("%d", &limit);
+    }
+
+    for (index = 0; index < limit;index++){
+        array[index] = 0;
+        printf("array[%d] is set to 0\n", index);
+    }
+        return 0;
 }
