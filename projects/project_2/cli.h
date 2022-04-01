be2cdf36a6dcbd1e1ff0a168984232054edc8735
HeroGamers <hero@herogamers.dev>
2022-04-01 10:31:38 +0200

    Add CLI header file

---
+++ projects/project_2/cli.h
@@ -0,0 +1,10 @@
+//
+// The CLI (command-line interface) for the program
+//
+
+#ifndef MACHINE_ORIENTED_PROGRAMMING_PROJECT_2_CLI_H
+#define MACHINE_ORIENTED_PROGRAMMING_PROJECT_2_CLI_H
+
+int drawCards();
+
+#endif //MACHINE_ORIENTED_PROGRAMMING_PROJECT_2_CLI_H
--- projects/project_2/commands.h
+++ projects/project_2/commands.h
@@ -1,9 +1,15 @@
 #ifndef MACHINE_ORIENTED_PROGRAMMING_PROJECT_2_COMMANDS_H
 #define MACHINE_ORIENTED_PROGRAMMING_PROJECT_2_COMMANDS_H
 
+#include "cli.h"
+
 // Command enum
 
-// function to print/draw the terminal window / GUI/CLI [SW]
+// [SW] function to print/draw the terminal window / GUI/CLI
+int SWCommand() {
+    drawCards();
+    return 0;
+}
 
 // function to handle commands
 
