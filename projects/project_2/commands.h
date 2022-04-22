d168e9a9701d28e41325f182b9186b0fdbcd4c76
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2022-04-22 10:14:07 +0200

    sine header stuff done

--- projects/project_2/commands.h
+++ projects/project_2/commands.h
@@ -4,14 +4,22 @@
 // Command enum
 
 // loads a deck of cards from a file, if no file is given, we load a new deck of cards
-int LDCommand();
+int LDCommand(char *fileName);
+
 // [SW] function to print/draw the terminal window / GUI/CLI
 int SWCommand();
+
 // Sl, splits the card deck into to piles
-int SlCommand();
+int SlCommand(int split);
+
 // SR command, shuffles the card deck
 int SRCommand();
 
+// SD saves the current deck of cards to a file
+int SDCommand(char *fileName);
+
+// QQ command, quits the program
+int QQCommand();
 
 
 #endif //MACHINE_ORIENTED_PROGRAMMING_PROJECT_2_COMMANDS_H
