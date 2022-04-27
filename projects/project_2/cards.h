35881d0ee8b0fcdbc1ec3ab34c52bd606d40a44d
HeroGamers <hero@herogamers.dev>
2022-04-27 21:03:46 +0200

    I am in unbearable pain

--- projects/project_2/cards.h
+++ projects/project_2/cards.h
@@ -5,4 +5,12 @@
 
 extern Card allCards[53];
 
+int shuffleCards(CardNode *cards);
+
+// takes from cards to deck
+int importCards(Card *cards);
+
+// takes from deck to cards
+int exportCards(Card *cards);
+
 #endif //MACHINE_ORIENTED_PROGRAMMING_PROJECT_2_CARDS_H
