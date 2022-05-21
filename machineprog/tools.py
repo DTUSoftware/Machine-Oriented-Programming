9617e37867983db573cbd512b10bc5170098f107
HeroGamers <15278940+HeroGamers@users.noreply.github.com>
2022-05-21 19:17:34 +0200

    convert to decimals and other fun stuff

--- machineprog/tools.py
+++ machineprog/tools.py
@@ -1,5 +1,5 @@
 class Binary:
-    def __init__(self, binary: str = "", bits: int = 8):
+    def __init__(self, binary: str = "0", bits: int = 8):
         self.binary = binary
         self.bits = bits
         self.binary_array = []
@@ -34,16 +34,16 @@ class Binary:
 
     def update_binary_array(self):
         self.binary_array = []
-        for i in range(len(self.binary)):
+        for i in range(self.bits):
             self.binary_array.append(self.binary[i])
 
     def update_binary_from_array(self):
         self.binary = ""
-        for i in range(len(self.binary_array)):
+        for i in range(self.bits):
             self.binary = self.binary + self.binary_array[i]
 
     def flip(self):
-        for i in range(len(self.binary_array)):
+        for i in range(self.bits):
             if self.binary_array[i] == "1":
                 self.binary_array[i] = "0"
             else:
@@ -51,38 +51,57 @@ class Binary:
         self.update_binary_from_array()
 
     def add(self, binary):
-        carry_on = False
-        added = False
-        for i in range(len(self.binary_array)):
-            j = abs(i - len(self.binary_array)) - 1
-            k = abs(i - len(binary.binary_array)) - 1
-            if k > len(binary.binary_array) or k < 0:
-                added = True
+        # make both binary ops same size
+        if self.bits != binary.bits:
+            if self.bits > binary.bits:
+                binary.bits = self.bits
+                binary.pad()
+                binary.update_binary_array()
+            else:
+                self.bits = binary.bits
+                self.pad()
+                self.update_binary_array()
 
+        operands_are_same = self.binary[0] == binary.binary[0]
+        same_operand = self.binary[0]
+
+        carry_on = False
+        for i in range(self.bits - 1, -1, -1):
             # print(f"i: {i}, j: {j}, k: {k}")
+            # print(self.binary_array)
+            # print(binary.binary_array)
 
             count = 0
-            if self.binary_array[j] == "1":
+            if self.binary_array[i] == "1":
                 count += 1
-            if not added and binary.binary_array[k] == "1":
+            if binary.binary_array[i] == "1":
                 count += 1
             if carry_on:
                 count += 1
 
             carry_on = False
             if count == 1:
-                self.binary_array[j] = "1"
+                self.binary_array[i] = "1"
             elif count == 2:
-                self.binary_array[j] = "0"
+                self.binary_array[i] = "0"
                 carry_on = True
             elif count == 3:
-                self.binary_array[j] = "1"
+                self.binary_array[i] = "1"
                 carry_on = True
+
         self.update_binary_from_array()
+
+        if operands_are_same and same_operand != self.binary[0]:
+            print("OVERFLOW - NUMBERS TOO BIG")
+
+    def subtract(self, binary):
+        binary.invert()
+        self.add(binary)
+        binary.invert()
 
     def invert(self):
         self.flip()
-        self.add(Binary("1"))
+        self.add(Binary("01"))
 
     def pad(self):
         if self.binary and len(self.binary) < self.bits:
@@ -91,10 +110,28 @@ class Binary:
             else:
                 self.binary = self.binary.rjust(self.bits, "1")
 
+    def to_decimal(self):
+        neg = False
+        if self.binary[0] == 1:
+            self.invert()
+            neg = True
+
+        decimal = 0
+        for i in range(len(self.binary)):
+            if self.binary[i] == "1":
+                # print(f"{decimal} + 2^{abs(i-self.bits)-1}")
+                decimal += 2**(abs(i-self.bits)-1)
+
+        if neg:
+            decimal = -decimal
+            self.invert()
+
+        return decimal
+
     def print(self):
         binary_string = ""
         j = 0
-        for i in range(len(self.binary)-1, -1, -1):
+        for i in range(self.bits-1, -1, -1):
             binary_string = self.binary[i] + binary_string
             j += 1
             if j == 4:
@@ -121,9 +158,15 @@ if __name__ == "__main__":
     binary.print()
 
     binary2 = Binary()
+    binary2.set_binary_from_decimal(-104)
+    binary2.print()
     binary2.set_binary_from_decimal(104)
     binary2.print()
-    binary2.set_binary_from_decimal(-104)
+    print(binary2.to_decimal())
+
+    binary3 = Binary()
+    binary3.set_binary_from_decimal(104)
+    binary2.subtract(binary3)
     binary2.print()
 
     hexNum = Hex()
